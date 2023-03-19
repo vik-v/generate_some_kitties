84ac7302f47abf950008f0f572440ccfdfd3753f
Viktoriya Selivanova <viktoriya.vonaviles2112@yandex.ru>
2023-03-19 21:04:08 +0300

    Add `--url`

--- README.md
+++ README.md
@@ -1,2 +1,151 @@
-# generate_some_kitties
-Tool for generating some stuff to test API
+# gensk
+#### Script to generate kitties  😺
+## Features
+- Generate kitties in JSON format
+- Check server status
+- Deploy to server via `POST` method using API
+- Clean database via `DELETE` method using API
+
+gensk is a script to help you full database with learning purposes.
+
+## Requirements
+gensk tested with next requiremetns, and it works
+
+| Pakage | Version |
+| ------ | ------ |
+| requests | 2.28.2 |
+| webcolors | 1.12 |
+
+## Installing
+### For linux system or WSL
+Get source
+```sh
+git clone git@github.com:vik-v/generate_some_kitties.git
+```
+Make virtual environment
+```sh
+cd generate_some_kitties
+python3 -m venv venv
+source venv/bin/activate
+```
+Install pip and dependencies
+```sh
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+### For MacOS
+...
+### For Windows
+...
+
+## Usage
+### Help
+Please, fill free to use [`-h` | `--help`]
+```sh
+usage: python3 gensk.py [-h] [-v [VERBOSITY]] [-s {http,https}] [-a [ADDRESS]] [-p [PORT]] [-r [RESOURCE]] [-u [URL]] [-g] [-l [{0,1,2,3}]] [-n [NUMBER_OF_CATS]] [-c | -d | -e]
+
+Script for generate and deploy kitties data via API
+
+options:
+  -h, --help            show this help message and exit
+  -v [VERBOSITY], --verbosity [VERBOSITY]
+                        Set verbosity level for logging (default: 1)
+
+URL configuration:
+  -s {http,https}, --schema {http,https}
+                        Select schema to use. (default: http)
+  -a [ADDRESS], --address [ADDRESS]
+                        Specify address. (default: 127.0.0.1)
+  -p [PORT], --port [PORT]
+                        Specify port number. (default: 8000)
+  -r [RESOURCE], --resource [RESOURCE]
+                        Specify API resource of endpoint. (default: cats/)
+  -u [URL], --url [URL]
+                        Fully specified URL to API. (default: )
+
+Generating kitties:
+  -g, --generate        Generate kitties and print in stdout. (default: False)
+  -l [{0,1,2,3}], --lesson-number [{0,1,2,3}]
+                        0: `Урок 5/15` 1: `Урок 9/15`2: `Урок 10/15` 3: `Урок 12/15`. (default: 0)
+  -n [NUMBER_OF_CATS], --number-of-cats [NUMBER_OF_CATS]
+                        Number of kitties to generate. (default: 10)
+
+API actions:
+  -c, --check-server    Basic check for endpoint availability. (default: False)
+  -d, --deploy          Deploy generated data via `POST` method. (default: False)
+  -e, --cleanup-db      Delete all entries in database via `DELETE` method. (default: False)
+```
+
+### Generating
+To generate kitties you may use [`-g` | `--generate`]
+```sh
+python3 gensk.py -g
+```
+By default it's generate 10 entries:
+```json
+[
+    {
+        "name": "Инки",
+        "color": "Black",
+        "birth_year": 2018
+    },
+    ...
+    {
+        "name": "Бруно",
+        "color": "Gray",
+        "birth_year": 2011
+    },
+    {
+        "name": "Дилан",
+        "color": "Black",
+        "birth_year": 2019
+    },
+]
+```
+Also, you can specify number of kitties to generate by using [`-n` |  `--number-of-cats`]
+```sh
+python3 gensk.py -g -n 1000
+```
+#### Check server response
+It will generate one thousand of kitties. What a pretty result!
+Checking endpoint availability [`-c` |  `--check-server`]
+```sh
+python3 gensk.py -c
+```
+By default it check: http://127.0.0.1:8000/cats/
+
+### Deploy data
+To deploy data use [`-d` | `--deploy`], as for generator you can specify number of kitties by using [`-n` |  `--number-of-cats`]
+```sh
+python3 gensk.py -d -n 5
+```
+It will generate five kitties and rocket launch them to server.
+
+### Cleanup database
+From the bottom of my heart i’ll had to say, that if you decide to genocide fluffy kitties, you may use [`-x` | `--cleanup-db`]
+```sh
+python3 gensk.py -x
+```
+It will totaly remove all kitties from you server. Burn in Hell, scratchy basters!!
+
+### Specifying custom URL or URL part
+For [`-c` | `--check-server`], [`-d` | `--deploy`], [`-x` | `--cleanup-db`] you may specify:
+| Option | Default value |
+| ------ | ------ |
+|[`-s` \| `--schema`]|`http`|
+|[`-a` \| `--address`]| `127.0.0.1`|
+|[`-p` \| `--port`]| `8000`|
+|[`-r` \| `--resource`]|`cats/`|
+```sh
+python3 gensk.py -d -n 5 -p 8080 -r api/v1/cats/
+python3 gensk.py -c -p 8080
+python3 gensk.py -x -a localhost
+```
+To specify fully URL, use [`-u` | `--url`]
+```sh
+python3 gensk.py -d -n 5 -u http://scratchy-baster9000.pythonanywhere.com/cats/
+```
+## License
+
+MIT
+**Free Software, Hell Yeah!**
--- gensk.py
+++ gensk.py
@@ -1,22 +1,25 @@
 import argparse
-import random
-import requests
-import logging
 import json
+import logging
+import random
+import sys
 from http import HTTPStatus
-from webcolors import CSS3_HEX_TO_NAMES
+from logging import DEBUG, ERROR, INFO
+from urllib.parse import urlparse
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
-)
+import requests
+
+from webcolors import CSS3_HEX_TO_NAMES
 
 SCHEMA = ['http', 'https']
 ADDRESS = '127.0.0.1'
 PORT = '8000'
-ENDPOINT = 'cats/'
+RESOURCE = 'cats/'
 NUMBER_OF_CATS = 10
-SPRINT_BASE = 'Спринт 8/18 → Тема 1/3: Django Rest Framework →'
+LESSON_NUMBER = (0, 1, 2, 3)
+VERBOSITY = {0: ERROR,
+             1: INFO,
+             3: DEBUG}
 
 INIT_DATA = {
     'name': ['Аврора', 'Агат', 'Агата', 'Адам', 'Адель', 'Айвори', 'Айк',
@@ -96,46 +99,92 @@ INIT_DATA = {
 }
 
 parser = argparse.ArgumentParser(
-    description='Script for generate and deploy kitties data via API',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    prog='python3 gensk.py',
+    description='Script for generate and deploy kitties data via API'
 )
 
-parser.add_argument('-s', '--schema', choices=SCHEMA, default=SCHEMA[0],
-                    help=('Select schema to use.')
+parser.add_argument('-v', '--verbosity', nargs='?', type=int,
+                    choices=VERBOSITY,
+                    const=VERBOSITY[1],
+                    default=VERBOSITY[1],
+                    help='Set verbosity level for logging'
                     )
 
-parser.add_argument('-a', '--address', nargs='?', default=ADDRESS,
-                    help='Specify address.')
+group_url_parts = parser.add_argument_group('URL configuration')
+group_url_parts.add_argument('-s', '--schema', choices=SCHEMA,
+                             default=SCHEMA[0],
+                             help=('Select schema to use.')
+                             )
+group_url_parts.add_argument('-a', '--address', nargs='?', default=ADDRESS,
+                             help='Specify address.')
+group_url_parts.add_argument('-p', '--port', nargs='?', default=PORT,
+                             help='Specify port number.')
+group_url_parts.add_argument('-r', '--resource', nargs='?', default=RESOURCE,
+                             help='Specify API resource of endpoint.')
+group_url_parts.add_argument('-u', '--url', nargs='?', type=str,
+                             const='', default='',
+                             help='Fully specified URL to API.')
 
-parser.add_argument('-p', '--port', nargs='?', default=PORT,
-                    help='Specify port number.')
+group_generating = parser.add_argument_group('Generating kitties')
+group_generating.add_argument('-g', '--generate', action='store_true',
+                              help='Generate kitties and print in stdout.')
+group_generating.add_argument('-l', '--lesson-number', nargs='?', type=int,
+                              choices=LESSON_NUMBER,
+                              const=LESSON_NUMBER[0],
+                              default=LESSON_NUMBER[0],
+                              help=('0: `Урок 5/15` 1: `Урок 9/15`'
+                                    '2: `Урок 10/15` 3: `Урок 12/15`.')
+                              )
+group_generating.add_argument('-n', '--number-of-cats',
+                              nargs='?', type=int,
+                              const=NUMBER_OF_CATS,
+                              default=NUMBER_OF_CATS,
+                              help=('Number of kitties to generate.')
+                              )
 
-parser.add_argument('-u', '--url', nargs='?',
-                    help='Fully specified URL to API.')
+group_actions = parser.add_argument_group(
+    'API actions').add_mutually_exclusive_group(required=False)
+group_actions.add_argument('-c', '--check-server', action='store_true',
+                           help='Basic check for endpoint availability.')
+group_actions.add_argument('-d', '--deploy', action='store_true',
+                           help='Deploy generated data via `POST` method.')
+group_actions.add_argument('-e', '--cleanup-db', action='store_true',
+                           help='Delete all entries in database via `DELETE` '
+                                 'method.'
+                           )
 
-parser.add_argument('-l', '--lesson-number', nargs='?', type=int, default=0,
-                    help=('0: `Урок 5/15` 1: `Урок 9/15`'
-                          '2: `Урок 10/15` 3: `Урок 12/15`.')
-                    )
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
-parser.add_argument('-g', '--generate', action='store_true',
-                    help='Generate kitties and print in stdout.')
+logging.basicConfig(
+    level=args.verbosity,
+    format=('%(asctime)s - [%(levelname)s] - %(name)s - '
+            '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s')
+)
 
-parser.add_argument('-n', '--number-of-cats',
-                    nargs='?', type=int, default=NUMBER_OF_CATS,
-                    help=('Number of kitties to generate.')
-                    )
 
-parser.add_argument('-c', '--check-server', action='store_true',
-                    help='Basic check for endpoint availability.')
+def url_validator(url: str) -> bool:
+    """Simple URL validator."""
+    try:
+        if not url.endswith('/'):
+            url += '/'
+        result = urlparse(url)
+        if result.scheme not in SCHEMA:
+            url = SCHEMA[0] + '://' + url
+            result = urlparse(url)
+            logging.debug(f'Schema not known. Set to `http://{url}`')
+        return all([result.scheme, result.netloc, result.path]), result, url
+    except Exception as e:
+        logging.error(e)
+        return False
 
-parser.add_argument('-d', '--deploy', action='store_true',
-                    help='Deploy generated data via `POST` method.')
 
-parser.add_argument('-r', '--cleanup-db', action='store_true',
-                    help='Delete all entries in database via `DELETE` method.')
-
-args = parser.parse_args()
+def pretty_json(data: list) -> json:
+    return json.dumps(data,
+                      sort_keys=False,
+                      indent=4,
+                      ensure_ascii=False,
+                      separators=(',', ': ')
+                      )
 
 
 def generate_data(sprint_lesson: int, number_of_cats: int) -> list:
@@ -143,100 +192,104 @@ def generate_data(sprint_lesson: int, number_of_cats: int) -> list:
 
     Meaning:
         0: 'Спринт 8/18 → Тема 1/3: Django Rest Framework → Урок 5/15'
+            Необходимые поля `name`, `color`, `birth_year`
+
         1: 'Спринт 8/18 → Тема 1/3: Django Rest Framework → Урок 9/15'
+            Необходимые поля: `name`, `color`, `birth_year`,
+                              `owner`, `achievements`
+            `color` указывается в шестнадцатеричном виде #00abc1
+
         2: 'Спринт 8/18 → Тема 1/3: Django Rest Framework → Урок 10/15'
+            Необходимые поля: `name`, `color`, `birth_year`,
+                              `owner`, `achievements`
+            `achievemens` = {`achievement_name`: ...}
+            Изменено отображение с использованием source=
+                `achievement_name = serializers.CharField(source='name')`
+
         3: 'Спринт 8/18 → Тема 1/3: Django Rest Framework → Урок 12/15'
+            Необходимые поля: `name`, `color`, `birth_year`,
+                              `owner`, `achievements`
+            `color` выбирается из списка выбора:
+                'Gray', 'Black', 'White', 'Белый', 'Ginger', 'Mixed'
+                `color = models.CharField(max_length=16, choices=CHOICES)`
     """
     cats = []
 
+    for _ in range(number_of_cats):
+        cats.append(
+            {
+                'name': random.choice(INIT_DATA.get('name')),
+                'color': random.choice(INIT_DATA.get('color'))[0],
+                'birth_year': random.randint(INIT_DATA.get('birth_year')[0],
+                                             INIT_DATA.get('birth_year')[1]),
+            }
+        )
     if sprint_lesson == 0:
-        for _ in range(number_of_cats):
-            cats.append(
-                {
-                    'name': random.choice(INIT_DATA['name']),
-                    'color': random.choice(INIT_DATA['color'])[0],
-                    'birth_year': random.randint(INIT_DATA['birth_year'][0],
-                                                 INIT_DATA['birth_year'][1]),
-                }
-            )
+        return cats
 
+    for cat in cats:
+        cat['owner'] = INIT_DATA.get('owner')
+        cat['color'] = random.choice(list(CSS3_HEX_TO_NAMES))
+        cat['achievements'] = random.choice(
+            [
+                [],
+                [{'name': random.choice(INIT_DATA.get('achievements'))}]
+            ]
+        )
     if sprint_lesson == 1:
-        for _ in range(number_of_cats):
-            cats.append(
-                {
-                    'name': random.choice(INIT_DATA['name']),
-                    'color': random.choice(list(CSS3_HEX_TO_NAMES)),
-                    'birth_year': random.randint(INIT_DATA['birth_year'][0],
-                                                 INIT_DATA['birth_year'][1]),
-                    'owner': INIT_DATA['owner'],
-                    'achievements': [
-                        {'name': random.choice(INIT_DATA['achievements'])}
-                    ]
-                }
-            )
+        return cats
 
+    for cat in cats:
+        cat['achievements'] = random.choice(
+            [
+                [],
+                [
+                    {'achievement_name': random.choice(
+                        INIT_DATA.get('achievements'))}
+                ]
+            ]
+        )
     if sprint_lesson == 2:
-        for _ in range(number_of_cats):
-            cats.append(
-                {
-                    'name': random.choice(INIT_DATA['name']),
-                    'color': random.choice(list(CSS3_HEX_TO_NAMES)),
-                    'birth_year': random.randint(INIT_DATA['birth_year'][0],
-                                                 INIT_DATA['birth_year'][1]),
-                    'owner': INIT_DATA['owner'],
-                    'achievements': [
-                        {'achievement_name':
-                         random.choice(INIT_DATA['achievements'])}
-                    ]
-                }
-            )
+        return cats
 
+    for cat in cats:
+        cat['color'] = random.choice(INIT_DATA.get('color'))[0]
     if sprint_lesson == 3:
-        for _ in range(number_of_cats):
-            cats.append(
-                {
-                    'name': random.choice(INIT_DATA['name']),
-                    'color': random.choice(INIT_DATA['color'])[0],
-                    'birth_year': random.randint(INIT_DATA['birth_year'][0],
-                                                 INIT_DATA['birth_year'][1]),
-                    'owner': INIT_DATA['owner'],
-                    'achievements': [
-                        {'achievement_name':
-                         random.choice(INIT_DATA['achievements'])}
-                    ]
-                }
-            )
-    return cats
+        return cats
 
 
-def check_server(endpoint: str) -> bool:
+def check_server(url: str) -> bool:
     """Check server availability."""
     try:
-        response = requests.get(endpoint)
+        response = requests.get(url)
         if response.status_code != HTTPStatus.OK:
             logging.warning(response.status_code)
             return False
+        logging.debug(response)
+        logging.info(f'Successful response from: {url}')
         return True
-    except requests.exceptions.ConnectionError as e:
+    except Exception as e:
         logging.error(e)
 
 
-def deploy(endpoint: str, cats: list) -> None:
+def deploy(url: str, cats: list) -> None:
     """Deploy data to server via POST method."""
     try:
         if cats:
             for cat in cats:
-                requests.post(endpoint, json=cat)
+                response = requests.post(url, json=cat)
+                if response.status_code != 201:
+                    logging.error(f'{response.status_code} -> {response.text}')
         else:
             logging.warning('Cats list is empty.')
     except Exception as e:
         logging.error(e)
 
 
-def cleanup_db(endpoint: str) -> None:
+def cleanup_db(url: str) -> None:
     """Delete all entries from database."""
     try:
-        response = requests.get(ENDPOINT)
+        response = requests.get(url)
         entries = []
         for entry in response.json():
             entries.append(entry.get('id'))
@@ -244,7 +297,7 @@ def cleanup_db(endpoint: str) -> None:
         logging.info(f'Entries in database: {len(entries)}')
 
         for entry in entries:
-            requests.delete(endpoint + str(entry))
+            requests.delete(url + str(entry))
 
         logging.info('Cleanup successfuly done.')
 
@@ -254,34 +307,53 @@ def cleanup_db(endpoint: str) -> None:
 
 def main() -> None:
     """Main function."""
-    url = f'{args.schema}://{ADDRESS}:{PORT}/{ENDPOINT}'
+    logging.debug(f'\n{args}')
 
-    if args.url:
-        url = args.url
+    args_url_parts = ('-s', '--schema', '-a', '--address', '-p', '--port',
+                      '-r', '--resource')
+    if any([arg in args_url_parts for arg in sys.argv[1:]]) and not any(
+            [args.check_server, args.deploy, args.cleanup_db]):
+        parser.error(
+            'You may use this option only with [-c | --check-server], '
+            '[-d | --deploy] or with [-e | --cleanup-db].\n'
+            'Like: gensk.py -c -p 80\n'
+            '      gensk.py --deploy --lesson-number 3 --number-of-cats 5'
+        )
+    elif args.url:
+        valide_url = url_validator(args.url)
+        if valide_url[0]:
+            url = valide_url[2]
+    else:
+        url = f'{args.schema}://{args.address}:{args.port}/{args.resource}'
+
+    args_gen_or_deploy = ('-l', '--lesson-number', '-n', '--number-of-cats')
+    if any([arg in args_gen_or_deploy for arg in sys.argv[1:]]) and not any(
+            [args.generate, args.deploy]):
+        parser.error(
+            'You may use this option only with [-g | --generate] or with '
+            '[-d | --deploy].\n'
+            'Like: gensk.py -g -l 3 -n 20\n'
+            '      gensk.py --deploy --lesson-number 3 --number-of-cats 5'
+        )
 
     if args.generate:
         print(
-            json.dumps(generate_data(args.lesson_number,
-                                     args.number_of_cats),
-                       sort_keys=False,
-                       indent=4,
-                       ensure_ascii=False,
-                       separators=(',', ': '))
+            pretty_json(generate_data(args.lesson_number,
+                                      args.number_of_cats))
         )
-    else:
-        parser.print_help()
 
     if args.check_server:
         check_server(url)
 
     if args.deploy:
         if check_server(url):
-            deploy(url, generate_data(args.lesson_number,
-                                      args.number_of_cats))
+            data = generate_data(args.lesson_number, args.number_of_cats)
+            deploy(url, data)
+            logging.debug(f'Deployed data: {pretty_json(data)}')
 
     if args.cleanup_db:
         if check_server(url):
-            cleanup_db()
+            cleanup_db(url)
 
 
 if __name__ == '__main__':
